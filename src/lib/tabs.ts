b0717ac4e691679763c8734836961cff3ed5b179
Matúš Mendel <matus@ametica.sk>
2022-09-06 22:58:45 +0200

    ✨ Multiple tab instances support

--- src/lib/tabs.ts
+++ src/lib/tabs.ts
@@ -1,34 +1,36 @@
-export const initializeTabs = () => {
-	const tabButtons = [...document.querySelectorAll('[data-tab-button]')];
+export const initializeTabs = (wrapperSelector) => {
+	const tabWrapper = document.querySelector(wrapperSelector);
+	
+	const tabButtons = [...tabWrapper.querySelectorAll('[data-tab-button]')];
 	const tabContents = []
 
 	for (const [index, tabButton] of tabButtons.entries()) {
 		const activeTabID = tabButton.getAttribute('data-tab-id');
-		const activeTabContent = document.querySelector(`[data-tab-content][data-tab-id=${activeTabID}]`)
+		const activeTabContent = tabWrapper.querySelector(`[data-tab-content][data-tab-id=${activeTabID}]`)
 
 		tabContents.push(activeTabContent);
 
 		if (index === 0) {
-			tabButton.classList.add('active');
+			tabButton?.classList?.add('active');
 		} else {
-			activeTabContent.classList.toggle('hidden');
+			activeTabContent?.classList.toggle('hidden');
 		}
 
 		tabButton.addEventListener('click', (e) => {
-			const activeTabButton = document.querySelector(`[data-tab-button].active`)
+			const activeTabButton = tabWrapper.querySelector(`[data-tab-button].active`)
 
 			if ( activeTabButton === tabButton ) {
 				return;
 			}
 
-			activeTabButton.classList.remove('active');
+			activeTabButton?.classList?.remove('active');
 
-			document.querySelectorAll(`[data-tab-content]`).forEach(tabContent => {
-				tabContent.classList.add('hidden');
+			tabWrapper.querySelectorAll(`[data-tab-content]`).forEach(tabContent => {
+				tabContent?.classList?.add('hidden');
 			})
 
-			tabButton.classList.add('active');
-			activeTabContent.classList.remove('hidden');
+			tabButton?.classList.add('active');
+			activeTabContent?.classList.remove('hidden');
 		})
 	}
 }
